830ba9b644db72a1cb0254ea5949a66a01bff354
Brayden Thistle <bthistle@huronperthcatholic.ca>
2026-01-04 23:46:02 +0000

    started program

---
+++ main.ts
@@ -0,0 +1,10 @@
+/**
+ * @author Brayden Thistle
+ * @version 1.0.0
+ * @date 2026-01-03
+ * @fileoverview this program 
+*/
+
+//declaring your 2 arrays
+const array1 = [13, -22, 82, 17, 96, 45, 23, 87, -12, -71];
+const array2 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
